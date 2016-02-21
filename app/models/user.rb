8cf2bb2e7530427acd850d9de3786c249c722c0f
drownedout <danielcdonnelly@gmail.com>
2016-02-21 15:41:49 -0500

    Added line in user model that prevents duplicates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,16 @@ class User < ActiveRecord::Base
 	paginates_per 50
 
 	def self.import(file)
+		@id_number = User.pluck(:id_number)
+
 		CSV.foreach(file.path, headers: true) do |row|
-			User.create! row.to_hash
-			Integer(row["company_id"])
-			Time.parse(row["ccs_date"])
-			Time.parse(row["ces_date"])
+				unless @id_number.include? Integer(row["id_number"])
+				User.create! row.to_hash
+				Integer(row["id_number"])
+				Integer(row["company_id"])
+				Time.parse(row["ccs_date"])
+				Time.parse(row["ces_date"])
+			end
 		end
 	end
 	
